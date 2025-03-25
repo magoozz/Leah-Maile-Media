943a2439c771e7a76eeaf9b808629ef7db307e6b
Megan Bartlett <megan.bartlett@elexicon.com>
2025-03-24 23:33:54 -0500

    hot fix

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,7 +5,7 @@ import 'bootstrap/dist/css/bootstrap-grid.min.css';
 export default function About() {
   return (
     <div className="About col-md-12">
-      <p>Here's the about page, what shall we put here?</p>
+      <p>Heres the about page, what shall we put here?</p>
       <Image
         src="/images/leahp-1.svg"
         alt="LeahP"
