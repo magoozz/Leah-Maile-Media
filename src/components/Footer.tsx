415bb58a300c1a91d3b215f970630062ba7da76c
Megan Bartlett <megan.bartlett@elexicon.com>
2025-03-31 11:18:43 -0500

    ig footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@
 import Image from 'next/image';
 import 'bootstrap/dist/css/bootstrap-grid.min.css';
 import Link from "next/link";
-import { FaBook } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
 
 export default function Footer() {
     return (
@@ -23,6 +23,14 @@ export default function Footer() {
             </div>
             <div className="footer-content">
                 <p>&copy; {new Date().getFullYear()} Leah Maile Media. All Rights Reserved.</p>
+                <a
+                    href="https://www.instagram.com/leahmailes/?igsh=OHJjaWxlNzBmZDNu#"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="instagram-mobile"
+                >
+                    <FaInstagram size={30} className="ig-icon" />
+                </a>
                 <p>Developed by <Link href="https://megan-bartlett.vercel.app/" target="_blank" rel="noopener noreferrer"> Megan Bartlett</Link></p>
             </div>
         </footer>
